1e7b697e9d2ef1896707db3332802556f907c50b
Eric Allam <rubymaverick@gmail.com>
2010-04-24 22:27:04 -0400

    can now toggle on/off searches.  Now just need to be able to manage the search terms a little better.  Idea: put a different background color for each search term to easily identify which tweets are from what search term

--- node.websocket.js/modules/userstream.js
+++ node.websocket.js/modules/userstream.js
@@ -4,17 +4,50 @@ var sys = require('sys'),
 var base64 = require('../../base64');
 
 var Module = this.Module = function(){
-  this.start = function(connection){
-    var chirpstream = http.createClient(80, 'chirpstream.twitter.com');
-    var username = pass.username;
-    var password = pass.password;
-    var auth = username + ':' + password;
-    auth = 'Basic ' + base64.encode(auth);
-    sys.puts(auth);
-    var request = chirpstream.request('GET', '/2b/user.json', {'host': 'chirpstream.twitter.com', 'Authorization': auth});
+  var chirp_host = 'chirpstream.twitter.com';
+  var username = pass.username;
+  var password = pass.password;
+  var auth = username + ':' + password;
+  auth = 'Basic ' + base64.encode(auth);
+  var path = '/2b/user.json';
+  var headers = {'host': chirp_host, 'Authorization': auth};
+  var live_responses = [];
+  
+  var syslog = function(str){
+    sys.debug(sys.inspect(str));
+  }
+  
+  this.make_request = function(connection, query_params){
+    // destroy any live responses and remove them from the live response queue
+    if(live_responses.length){
+      for (var i=0; i < live_responses.length; i++) {
+        if(live_responses[i]){
+          syslog("killing response " + i);
+          live_responses[i].pause();
+          syslog("killed response " + i);
+          live_responses[i] = undefined;          
+        }
+      }
+    }
+    
+    syslog("Making new request");
+    syslog("path: " + path);
+    syslog("headers: " + JSON.stringify(headers));
+    
+    var query_string = "";
+    if(query_params){
+      var pairs = [];
+      for(key in query_params){
+        pairs.push(key + "=" + query_params[key]);
+      }
+      query_string = "?" + pairs.join('&')
+    }
+    syslog('query params: ' + query_string);
+    var chirpstream = http.createClient(80, chirp_host);
+    var request = chirpstream.request('GET', path + query_string, headers);
     request.should_keep_alive = true;
     request.addListener('response', function (response) {
-        if(response.statusCode == 200){
+      if(response.statusCode == 200){
         sys.debug(sys.inspect('STATUS: ' + response.statusCode));
         sys.debug(sys.inspect('HEADERS: ' + JSON.stringify(response.headers)));
         response.setEncoding('utf8');
@@ -22,15 +55,19 @@ var Module = this.Module = function(){
           sys.debug(sys.inspect(chunk));
           if(chunk != '\r\n'){
             connection.send(chunk);
-          }else{
-            // each chunk
           }
-        });    
-      }
+        }); // end addListener data
 
-    });
+        live_responses.push(response);
+      } // end if status code == 200
+
+    }); // end of the addListener response
 
     request.end();    
+  }
+  
+  this.start = function(connection){
+    this.make_request(connection);
   }
 };
 
@@ -38,7 +75,22 @@ Module.prototype.onData = function(data, connection){
   if (data == 'start'){
     sys.debug(sys.inspect('in start connection'));
     this.start(connection);
-  }  
+  }else if (data.length){
+    try {
+      sys.debug(sys.inspect(data));
+      var message = JSON.parse(data);
+      switch(message['method']) {
+        case 'new_search':
+          sys.debug(sys.inspect('new_search: ' + message['search']));
+          this.make_request(connection, {'track': message['search']});
+          break;
+        default:
+        // do nothing
+      }
+    } catch(e){
+      // could not parse
+    }
+  }
 };
 
 Module.prototype.onDisconnect = function(connection){
