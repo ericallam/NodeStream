3850187af3e5e1b2a60ffec3eff56f34ef8a5199
Eric Allam <rubymaverick@gmail.com>
2010-05-03 15:48:05 -0400

    userstream -> twitter-userstream

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var sys = require('sys'),
     http = require('http'),
-    io = require('./socket.io/lib/socket.io'),
-    userstream = require('./lib/userstream');
+    io = require('./socket.io/lib/socket.io');
+var kiwi = require('kiwi')
+var UserStream = kiwi.require('twitter-userstream').UserStream
     
 server = http.createServer(function(req, res){
     // your normal server code
@@ -12,13 +13,55 @@ server = http.createServer(function(req, res){
 
 server.listen(8080);
 
+var send_to_client = function(client, obj, type){
+  client.send(JSON.stringify({type: type, payload: obj}));
+}
+
+var credentials = process.argv[2].split(':');
+
+var username = credentials[0];
+var password = credentials[1];
+
+var userstream = new UserStream({
+  username: username,
+  password: password,
+  debug: false
+});
+
 // socket.io, I choose you
 var listener = io.listen(server, {
 		
 	onClientConnect: function(client){
 	  sys.puts("onClientConnect: ");
     sys.puts(sys.inspect(client));
-    userstream.make_request(client);
+    
+    userstream
+      .onFriends(function(friends){
+        send_to_client(client, friends, "friends");
+      })
+      .onStatus(function(status){
+        send_to_client(client, status, "status");
+      })
+      .onRetweet(function(retweet){
+        send_to_client(client, retweet, "retweet");
+      })
+      .onFavorite(function(favorite){
+        send_to_client(client, favorite, "favorite");
+      })
+      .onUnfavorite(function(unfavorite){
+        send_to_client(client, unfavorite, "unfavorite");
+      })
+      .onFollow(function(follow){
+        send_to_client(client, follow, "follow");
+      })
+      .onUnfollow(function(unfollow){
+        send_to_client(client, unfollow, "unfollow");
+      })
+      .onBlock(function(block){
+        send_to_client(client, block, "block");
+      });
+    
+    userstream.stream();
 	},
 	
 	onClientDisconnect: function(client){
